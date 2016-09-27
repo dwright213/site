ead43bf8c1b75b3232b8cae90d9f5c41b39e63bc
Dan Wright <muscle.crab.13@gmail.com>
2016-09-26 21:26:50 -0700

    package.json updates

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,10 @@ gulp.task('styles', function() {
 	return gulp.src('static/sass/vendor/bootstrap/bootstrap.scss')
 	.pipe(sass())
 	.pipe(postcss([require('postcss-flexbugs-fixes')]))
-  .pipe(autoprefixer({
+  	.pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
   }))
   .pipe(gulp.dest('static/css/vendor'))
 });
+
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
 ## dan's site
 
-Because as a developer, who do you think you're fooling if you don't have a minimalist blog type site where you rant about open source in roboto font?
-
-Figured I would change things up though, and not use Jekyll.
+WEBSITES, AMIRITE?

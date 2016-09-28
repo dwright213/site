6bf28a402524c1522f0ada654eb9e45f357bb3af
Dan Wright <muscle.crab.13@gmail.com>
2016-09-27 22:10:09 -0700

    minify and purify our stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
+var minify = require('gulp-clean-css');
+var purify = require('gulp-purifycss');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var flexbugs = require('postcss-flexbugs-fixes');
@@ -10,11 +12,13 @@ var flexbugs = require('postcss-flexbugs-fixes');
 gulp.task('styles', function() {
 	return gulp.src('static/sass/vendor/bootstrap/bootstrap.scss')
 	.pipe(sass())
+	.pipe(purify(['./static/js/**/*.js', './templates/*.html']))
 	.pipe(postcss([require('postcss-flexbugs-fixes')]))
   	.pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
-  }))
+	}))
+	.pipe(minify())
   .pipe(gulp.dest('static/css/vendor'))
 });
 

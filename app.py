790ad0a930544e5bbdbd67ae14293e5f40c3d29a
Dan Wright <muscle.crab.13@gmail.com>
2016-10-16 22:58:47 -0700

    further experiment with pytumblr

--- app.py
+++ app.py
@@ -36,5 +36,15 @@ def get_tumblr_info():
     )
     return jsonify(client.info())
 
+@app.route('/tumblr_photos')
+def get_tumblr_photos():
+    client = pytumblr.TumblrRestClient(
+        app.config['CONSUMER_KEY'],
+        app.config['CONSUMER_SECRET'],
+        app.config['OAUTH_TOKEN'],
+        app.config['OAUTH_SECRET']
+    )
+    return jsonify(client.posts('classicsretold.tumblr.com', type='photo', limit=1000, filter='text'))
+
 if __name__ == "__main__":
 	app.run()

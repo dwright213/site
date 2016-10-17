46297e08c645d1b50a99d878594b0cd858227db8
Dan Wright <muscle.crab.13@gmail.com>
2016-10-16 22:31:11 -0700

    successfully talk to tumblr

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, g, abort
+from flask import Flask, render_template, request, g, abort, jsonify
+import pytumblr
+
 app = Flask(__name__)
+app.config.from_pyfile('settings.cfg')
 
 @app.before_request
 def set_up_nav():
@@ -22,6 +25,16 @@ def fotos():
 @app.route('/etc')
 def etc():
 	return render_template('etc.html')
+
+@app.route('/tumblr_info')
+def get_tumblr_info():
+    client = pytumblr.TumblrRestClient(
+        app.config['CONSUMER_KEY'],
+        app.config['CONSUMER_SECRET'],
+        app.config['OAUTH_TOKEN'],
+        app.config['OAUTH_SECRET']
+    )
+    return jsonify(client.info())
 
 if __name__ == "__main__":
 	app.run()
